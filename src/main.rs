dd4e06bd8f754167970018a8cd5ca929287c6c36
Jay Patel <jay037493@gmail.com>
2024-01-25 14:42:54 +0530

    Chapter 2 done

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,46 @@
-use std::io;
+use rand::Rng;
+use std::{cmp::Ordering, io};
 
 fn main() {
+    //Practice code
+    let x = 5;
+    let y = 10;
+    println!("x = {x} and y + 2 = {}", y + 2);
+
+    //Guess the number game starts from here
     println!("Guess the number");
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    // println!("The secret number is: {secret_number}");
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {guess}");
+        let mut guess = String::new();
+        //mutable
+        //(::new line indicates that new is an associated function)
+        //(line has created a mutable variable that is currently bound to a new, empty instance of a String)
+
+        io::stdin()
+            .read_line(&mut guess) // & indicates that this argument is a reference
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, //The underscore is a catchall value , continue tells the program to go to the next iteration of the loop
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            //cmp here compares the two values
+            //match is like when() in kotlin
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You won!");
+                break;
+            }
+        }
+    }
 }
