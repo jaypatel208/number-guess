3789b71bfe607708693fa599958c184bd0a69835
Jay Patel <jay037493@gmail.com>
2024-02-07 19:20:16 +0530

    slice done in rust

--- src/the-slice-type/src/main.rs
+++ src/the-slice-type/src/main.rs
@@ -1,15 +1,23 @@
 fn main() {
     let mut s = String::from("hello world");
-    let word = first_word(&s);
-    s.clear()
+    let _word = first_word(&s);
+    println!("First word is {}", _word);
+    s.clear();
+
+    let song_name = String::from("Rait zara si");
+    let rait = &song_name[..3]; // ..3 & 0..3 both are same
+    let zara = &song_name[5..8];
+    let si = &song_name[10..]; // 10.. is same as 10..len // .. equals to 0..len
+
+    print!("Song name {} {} {}", rait, zara, si);
 }
 
-fn first_word(s: &str) -> usize {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i    ;
+            return &s[0..i];
         }
     }
-    s.len()
+    &s[..]
 }
