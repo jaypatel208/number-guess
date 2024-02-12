d438fd7da24e14b456514e9d1e25c91d48a97da0
Jay Patel <jay037493@gmail.com>
2024-02-12 12:58:23 +0530

    The match Control Flow Construct done

--- src/enum-examples/src/main.rs
+++ src/enum-examples/src/main.rs
@@ -1,8 +1,42 @@
 fn main() {
-    println!("Hello, world!");
+    let home = IpAddrKind::V4(127, 0, 0, 1);
+    let loopback = IpAddrKind::V6(String::from("::1"));
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+}
+
+fn plus_one(x:Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i+1),
+    }
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
 }
 
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
 }
